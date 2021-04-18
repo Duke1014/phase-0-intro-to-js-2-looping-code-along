8a67a19ce27fde36d1c9f4e85ac8d72bd4d3b0d5
duke1014 <duke.norsworthy@gmail.com>
2021-04-17 20:00:42 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+// writeCards(["George", "Fred", "Harry"], 'birthday');
+const thanks = []
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {   
+        thanks.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thanks;
+}
+
+function countDown(n = 10) {
+    while (n >= 0) {
+        console.log(n--);
+    }
+}
